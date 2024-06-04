93edb7df2c4c61e0b1bb9111ec4d13271af2cf1f
Kirthigesan <kirthi101996@gmail.com>
2024-06-04 17:54:36 +0200

    create footer file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Header />
       <section></section>
 
-      <footer></footer>
+      <Footer />
     </>
   );
 }
