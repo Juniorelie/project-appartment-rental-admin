ae941252674ace8c817021f8ca5ab707af173115
Kirthigesan <kirthi101996@gmail.com>
2024-06-05 16:51:05 +0200

    add delete function

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,14 @@
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-
+import Main from "./components/Main/Main.jsx";
 
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="Header">
-          <Header />
-        </div>
-
-        <div className="Section">
- 
-        </div>
-
-        <div className="Footer">
-          <Footer />
-        </div>
-      </div>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import AppartmentalJson from "./../../assets/Appartmental.json";
+function Main() {
+  const [rooms, setRooms] = useState(AppartmentalJson);
+
+  function handleDeleteCard(roomId) {
+    const filteredRoom = rooms.filter((room) => room.id !== roomId);
+    setRooms(filteredRoom);
+  }
+  return (
+    <section>
+      {rooms.map((room) => {
+        return (
+          <article key={room.id}>
+            <h1>
+              {room.city}, {room.country}
+            </h1>
+
+            <button onClick={() => handleDeleteCard(room.id)}>Delete</button>
+          </article>
+        );
+      })}
+    </section>
+  );
+}
+
+export default Main;
