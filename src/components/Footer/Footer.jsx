cf034f4c2b6e796cb60e9cc43b439efaec73e013
Kirthigesan <kirthi101996@gmail.com>
2024-06-07 17:57:23 +0200

    add style to footer and header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer" id="Footer">
-      <a href="https://github.com/Juniorelie/project-appartment-rental-admin">
+      <a className='linkClass' href="https://github.com/Juniorelie/project-appartment-rental-admin">
         <i className="fa-brands fa-github"></i>
         GitHub</a>
     </footer>
