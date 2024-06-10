53fbd1333afb837b458eaf8ca36b14f3a7a10f16
Kaiser_Elie <mbakopjuniorelie@gmail.com>
2024-06-10 17:59:35 +0200

    added link to home to Ironhack Rental

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,20 +4,30 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="header" id="header">
-      <h1>Ironhack Rental</h1>
+      <Link to={"/"}>
+        <h1>Ironhack Rental</h1>
+      </Link>
       <nav className="Navbar">
         <ul>
           <li>
-            <Link className="linkClass" to={"/"}>Home</Link>
+            <Link className="linkClass" to={"/"}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link className="linkClass" to={"/about"}>About US</Link>
+            <Link className="linkClass" to={"/about"}>
+              About US
+            </Link>
           </li>
           <li>
-            <Link className="linkClass" to={"/addnewroom"}>Add New IronDom</Link>
+            <Link className="linkClass" to={"/addnewroom"}>
+              Add New IronDom
+            </Link>
           </li>
           <li>
-            <Link className="linkClass" to={"/favourites"}>Favourites</Link>
+            <Link className="linkClass" to={"/favourites"}>
+              Favourites
+            </Link>
           </li>
         </ul>
       </nav>
